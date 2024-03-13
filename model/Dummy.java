0e6b2765de127bb007f85f69cd4dc584bad5cc37
Will <ymailwill@gmail.com>
2024-03-13 17:19:38 -0400

    Dummy test
    Signed-off-by: Will <ymailwill@gmail.com>

--- model/Dummy.java
+++ model/Dummy.java
@@ -1,7 +1,15 @@
 package model;
 
+
 public class Dummy {
+
+    int test = 1;
+    
     public Dummy(){
-        int test = 1;
+        
     }
-}dfsfsdf
+
+    public int dummyMath(int dummy){
+        return dummy + test;
+    }
+}
